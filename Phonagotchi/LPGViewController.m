ecf3d7718586cbed2313f2b2ae715aa69bd53c2b
Steven Masuch <smasuch@electricmilk.com>
2014-07-27 14:31:31 -0700

    Add files.

---
+++ Phonagotchi/LPGViewController.m
@@ -0,0 +1,50 @@
+//
+//  LPGViewController.m
+//  Phonagotchi
+//
+//  Created by Steven Masuch on 2014-07-26.
+//  Copyright (c) 2014 Lighthouse Labs. All rights reserved.
+//
+
+#import "LPGViewController.h"
+
+@interface LPGViewController ()
+
+@property (nonatomic) UIImageView *petImageView;
+
+@end
+
+@implementation LPGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    self.view.backgroundColor = [UIColor colorWithRed:(252.0/255.0) green:(240.0/255.0) blue:(228.0/255.0) alpha:1.0];
+    
+    self.petImageView = [[UIImageView alloc] initWithFrame:CGRectZero];
+    self.petImageView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    self.petImageView.image = [UIImage imageNamed:@"default"];
+    
+    [self.view addSubview:self.petImageView];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.petImageView
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.petImageView
+                                                          attribute:NSLayoutAttributeCenterY
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterY
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    
+}
+
+@end
